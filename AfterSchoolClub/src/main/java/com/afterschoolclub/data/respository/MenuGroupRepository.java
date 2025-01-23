bc7f429754cc825d7f8a0986118008d004806c35
AdamH2001 <60469947+AdamH2001@users.noreply.github.com>
2025-01-23 18:40:44 +0000

    Delete AfterSchoolClub directory

--- AfterSchoolClub/src/main/java/com/afterschoolclub/data/respository/MenuGroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.afterschoolclub.data.respository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.afterschoolclub.data.MenuGroup;
-
-public interface MenuGroupRepository extends CrudRepository<MenuGroup, Integer> {
-
-}
