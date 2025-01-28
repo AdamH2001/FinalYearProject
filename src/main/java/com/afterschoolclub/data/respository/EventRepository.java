e73aae8556b3a9489367ff7e5b5ec454f1c554e3
AdamH2001 <adam@hattonsplace.co.uk>
2025-01-28 12:19:06 +0000

    Added method to return events inbetween two dates

--- src/main/java/com/afterschoolclub/data/respository/EventRepository.java
+++ src/main/java/com/afterschoolclub/data/respository/EventRepository.java
@@ -1,11 +1,17 @@
 package com.afterschoolclub.data.respository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.afterschoolclub.data.Event;
+import com.afterschoolclub.data.ParentalTransaction;
 
 public interface EventRepository extends CrudRepository<Event, Integer> {
 	List<Event> findAll();
+	
+	@Query("SELECT * FROM event WHERE start_date_time >= :startDate AND start_date_time < :endDate ORDER BY start_date_time") 
+	List<Event> findEventsBetweenDates(LocalDate startDate, LocalDate endDate);	
 }
